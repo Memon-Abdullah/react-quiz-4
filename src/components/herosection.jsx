7bd8b48085f799526902cfa4288df9fe4ecab5b7
Abdullah Memon <abdullahmemon.bscsf21@iba-suk.edu.pk>
2023-11-20 14:20:22 +0500

    initail commit

---
+++ src/components/herosection.jsx
@@ -0,0 +1,13 @@
+// method two arraw function
+
+import React from 'react'; // Added import statement
+
+const HeroSection = () => {
+  return (
+    <>
+      <h1>Hero Section</h1>
+    </>
+  )
+}
+
+export default HeroSection; // Export the component
